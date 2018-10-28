ae2e9f3aabb2736f6cf66c64c1b3dc1cf1631b0c
zara <zarakhanna@gmail.com>
2018-10-28 12:31:11 +0800

    Added autoHide: false to the action

--- chatbot_utility.js
+++ chatbot_utility.js
@@ -45,6 +45,7 @@ function display_prompt(prompt) {
         buttons.push({text: prompt.content[i],value: prompt.content[i]})
       }
       return botui.action.button({
+        autoHide: false,
         delay: 50,
         action: buttons
       });
